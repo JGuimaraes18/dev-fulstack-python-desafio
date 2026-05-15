aaac4af8c1eed93169db830f99e3ecf094d91a52
JGuimaraes18 <jpedro.guimaraes@hotmail.com>
2026-05-15 16:56:15 -0300

    feat(signals): create default roles and assign permissions on migrate

--- backend/apps/accounts/signals.py
+++ backend/apps/accounts/signals.py
@@ -1,11 +1,38 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
 
-@receiver(post_migrate)
+@receiver(post_migrate, dispatch_uid="create_roles_signal")
 def create_roles(sender, **kwargs):
-    roles = ["ADMIN", "VENDEDOR"]
 
-    for role in roles:
-        Group.objects.get_or_create(name=role)
+    seller_group, _ = Group.objects.get_or_create(name="SELLER")
+    admin_group, _ = Group.objects.get_or_create(name="ADMIN")
+
+    seller_permissions = Permission.objects.filter(
+        codename__in=[
+            "add_customer",
+            "change_customer",
+            "view_customer",
+            "add_sale",
+            "change_sale",
+            "view_sale",
+            "add_saleitem",
+            "change_saleitem",
+            "view_saleitem",
+            "view_product",
+        ]
+    )
+
+    seller_group.permissions.set(seller_permissions)
+
+    admin_permissions = Permission.objects.exclude(
+        content_type__app_label__in=[
+            "auth",
+            "admin",
+            "contenttypes",
+            "sessions",
+        ]
+    )
+
+    admin_group.permissions.set(admin_permissions)
