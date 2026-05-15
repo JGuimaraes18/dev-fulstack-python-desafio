14f5c5bbc193d80a5282dd4017e29519c0f386db
JGuimaraes18 <jpedro.guimaraes@hotmail.com>
2026-05-15 15:57:09 -0300

    refactor: move custom permission classes from accounts to core for centralization

--- backend/apps/accounts/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsAdminUserRole(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name="ADMIN").exists()
---
+++ backend/core/permissions.py
@@ -0,0 +1,39 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdminUserRole(BasePermission):
+    # Configuation for ADMIN group permissions
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="ADMIN").exists()
+    
+
+class IsSeller(BasePermission):
+    # Configuation for SELLER group permissions
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="SELLER").exists() 
+    
+
+class IsOwnerSale(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        # ADMIN access to all sales
+        if request.user.groups.filter(name="ADMIN").exists():
+            return True
+
+        # SELLER only access their own sales
+        return obj.seller.user == request.user   
+
+
+class IsSelfOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        # ADMIN permissions allow all actions
+        if request.user.groups.filter(name="ADMIN").exists():
+            return True
+
+        # SELLER permissions limited actions
+        return obj == request.user
