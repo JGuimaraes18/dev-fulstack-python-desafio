721ab5ee1b7142b32d4a13ff27cacf245d90edad
JGuimaraes18 <jpedro.guimaraes@hotmail.com>
2026-05-18 16:36:05 -0300

    feat(frontend/products): update sellers service integration

--- frontend/src/services/sellerService.ts
+++ frontend/src/services/sellerService.ts
@@ -2,6 +2,6 @@ import { api } from "./api"
 import type { Seller } from "../types/Seller";
 
 export async function getSellers(): Promise<Seller[]> {
-  const response = await api.get<Seller[]>("/customers/");
+  const response = await api.get<Seller[]>("/sellers/");
   return response.data;
 }
