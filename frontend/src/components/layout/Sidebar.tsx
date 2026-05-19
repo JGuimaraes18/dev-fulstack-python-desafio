c4fbc118b6a2bf56118652bbb7e15077dc25ecf6
João Guimarães <jpedro.guimaraes@hotmail.com>
2026-05-18 23:46:48 -0300

    style: improve sidebar layout and styling

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,21 +1,31 @@
 import { NavLink } from "react-router-dom";
 import { ChevronRight, Percent, ShoppingCart } from "lucide-react";
 
-export default function Sidebar() {
-  const baseClass =
-    "flex items-center justify-between px-3 py-3 transition-all duration-200";
+interface Props {
+  isOpen: boolean;
+}
 
-  const activeClass = "bg-slate-400 text-teal-900 font-bold shadow";
+export default function Sidebar({ isOpen }: Props) {
+  const baseClass =
+    "text-xs flex items-center justify-between px-2 py-2 transition-all duration-200";
+
+  const activeClass = "bg-slate-400 text-teal-900 font-semibold shadow";
   const inactiveClass =
     "text-teal-700 bg-white hover:bg-gray-200";
 
   const items = [
     { label: "Vendas", icones: ShoppingCart, to: "/" },
-    { label: "Comissões", icones: Percent , to: "/commissions" },
+    { label: "Comissões", icones: Percent, to: "/comissoes" },
   ];
 
   return (
-    <div className="bg-slate-200 border-r w-50">
+    <div
+      className={`
+        bg-slate-200 border-r 
+        transition-all duration-300 
+        ${isOpen ? "w-30" : "w-10"}
+      `}
+    >
       <nav className="space-y-2 py-2">
         {items.map((item) => (
           <NavLink
@@ -28,14 +38,16 @@ export default function Sidebar() {
             }
           >
             <div className="flex items-center gap-3">
-              <item.icones size={18} />
-              <span>{item.label}</span>
+              <item.icones size={16} />
+              {isOpen && <span>{item.label}</span>}
             </div>
 
-            <ChevronRight
-              size={22}
-              className="text-gray-400"
-            />
+            {isOpen && (
+              <ChevronRight
+                size={16}
+                className="text-gray-800"
+              />
+            )}
           </NavLink>
         ))}
       </nav>
