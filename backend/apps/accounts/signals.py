1881360ea1d093f630277c0dec571f7fbf5212c1
João Guimarães <jpedro.guimaraes@hotmail.com>
2026-05-20 00:07:11 -0300

    fix(accounts): resolve exception in signals during superuser creation

--- backend/apps/accounts/signals.py
+++ backend/apps/accounts/signals.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.models import Group, Permission
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 from django.dispatch import receiver
 
 
@@ -36,3 +37,17 @@ def create_roles(sender, **kwargs):
     )
 
     admin_group.permissions.set(admin_permissions)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL, dispatch_uid="add_superuser_to_admin_group_signal")
+def add_superuser_to_admin_group(sender, instance, created, **kwargs):
+    # Executa apenas se o usuário acabou de ser criado e se ele for superuser
+    if created and instance.is_superuser:
+        try:
+            # O grupo ADMIN já foi criado no post_migrate acima, então damos apenas um .get()
+            admin_group = Group.objects.get(name="ADMIN")
+            instance.groups.add(admin_group)
+            print(f"Sucesso: Superusuário {instance.email} vinculado ao grupo ADMIN.")
+        except Group.DoesNotExist:
+            print("Aviso: O grupo ADMIN não foi encontrado para vincular o superusuário.")
+        except Exception as e:
+            print(f"Erro ao vincular superusuário ao grupo: {e}")
