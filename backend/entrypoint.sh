7f2672b3b1eba852a3b393ff5ee475dbd66bec07
João Guimarães <jpedro.guimaraes@hotmail.com>
2026-05-19 22:58:40 -0300

    chore: refine production setup for Docker, Django

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,27 +10,12 @@ python manage.py collectstatic --noinput
 
 echo "Creating superuser (if not exists)..."
 
-python manage.py shell << EOF
-import os
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-if email and password:
-    if not User.objects.filter(email=email).exists():
-        User.objects.create_superuser(
-            email=email,
-            password=password
-        )
-        print("Superuser created")
-    else:
-        print("Superuser already exists")
-else:
-    print("Superuser env not set, skipping")
-EOF
+if [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    export DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-admin}
+    python manage.py createsuperuser --noinput || echo "Superuser already exists or failed to create."
+else
+    echo "Superuser env not set, skipping"
+fi
 
 echo "Starting Gunicorn..."
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
